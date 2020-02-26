048b105214cc32aedea3bfd01c36eb758ac3cd9f
github username <randyherasme6@gmail.com>
2020-02-26 14:40:45 -0500

    sign up with token implement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
     def create
         @user = User.create(user_params)
         if @user.valid?
-          render json: { user: UserSerializer.new(@user) }, status: :created
+          @token = encode_token(user_id: @user.id)
+          render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
         else
           render json: { error: 'failed to create user' }, status: :not_acceptable
         end
@@ -10,6 +13,6 @@ class UsersController < ApplicationController
     
       private
       def user_params
-        params.require(:user).permit(:email, :password, :name)
+        params.permit(:email, :password, :name)
       end
 end
