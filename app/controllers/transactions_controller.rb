0497c4e816d9058b24907b90ef2520abf9adaa88
github username <randyherasme6@gmail.com>
2020-03-02 14:19:42 -0500

    add balanace deduction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,15 @@
 class TransactionsController < ApplicationController
+
+    def create 
+        @transaction = Transaction.create(transaction_params)
+        current_balance = current_user.balance - @transaction.cost
+        current_user.update(balance: current_balance)
+        render json: { transaction: @transaction, current_balance: current_balance }
+    end 
+
+    private 
+
+    def transaction_params 
+        params.require(:transaction).permit(:ticker, :quantity, :cost, :user_id)
+    end 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :email, :name, :balance, :transactions
+  attributes :email, :name, :balance, :transactions, :id
 end
